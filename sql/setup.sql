15192561834e0998033f80bf952a92c1e7b5c056
armaBurton <armanarma@armaburton.com>
2022-03-31 12:47:19 -0700

    tables have been defined

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,15 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS gweets CASCADE;
+
+CREATE TABLE users (
+  gh_username TEXT NOT NULL PRIMARY KEY,
+  gh_photo_url TEXT NOT NULL
+);
+
+CREATE TABLE gweets (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  text TEXT NOT NULL
+  username TEXT REFERENCES users(gh_username)
+);
