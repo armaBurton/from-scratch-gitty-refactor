54b5590f8d86d9dbebee6c5c871972c76343c75c
armaBurton <armanarma@armaburton.com>
2022-03-31 15:06:30 -0700

    first test passes, app redirects on load

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,16 +3,16 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-jest.mock('..lib/middleware/authenticate.js', () => {
-  return (req, res, next) => {
-    req.user = {
-      username: 'test_user',
-      photoUrl: 'http://image.com/image.png',
-    };
+// jest.mock('..lib/middleware/authenticate.js', () => {
+//   return (req, res, next) => {
+//     req.user = {
+//       username: 'test_user',
+//       photoUrl: 'http://image.com/image.png',
+//     };
 
-    next();
-  };
-});
+//     next();
+//   };
+// });
 
 describe('from-scratch-gitty routes', () => {
   beforeEach(() => {
@@ -25,8 +25,15 @@ describe('from-scratch-gitty routes', () => {
 
   it('should redirect to the github oath page upon load', async () => {
     const req = await request(app)
-      .get('/api/v1/auth');
+      .get('/api/v1/auth/login');
 
+    expect(req.header.location).toMatch(
+      `https://github.com/login/oauth/authorize?client_id=fca10e824847706829ae&scope=user&redirect_uri=http://localhost:7890/api/v1/auth/login/callback`
+    );
     
   });
 });
+
+// /https:\/\/github.com\/login\/oauth\/authorize?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/auth\/login\/callback/i
+
+
